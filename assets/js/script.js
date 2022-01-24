8cea742f128b6d540cb3d16d6d67a1243bc1966b
Mets0811 <melissa0811@icloud.com>
2022-01-23 19:30:55 -0500

    final comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-//This section is for the Momentum, I mean Moment.js 
+// the Momentum, I mean Moment.js 
 
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
 
-//This section is for the hour variables in the schedule
+// hour variables in the schedule
 
 var nineAm = $("#9am");
 var tenAm = $("#10am");
